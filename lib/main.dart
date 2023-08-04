eeffac5fc60117df82e16f0f672d191fab06cd61
Luidi Matheus Silva de Oliveira <luidimatheus.gft@sulamerica.com.br>
2023-08-04 20:19:51 -0300

    Adding texts and colunm

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        color: Colors.green,
-        child: Text("Hello world!"),
-        alignment: Alignment.center,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.green,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Can go in!", style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.w700
+          ),),
+          Text("0", style: TextStyle(
+            fontSize: 100,
+            color: Colors.white
+          ),)
+        ],
       ),
     );
   }
