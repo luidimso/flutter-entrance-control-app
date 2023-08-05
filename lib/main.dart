ec160a0f051474594c8b56891f85c9e119921882
luidimso <luidi_mso@outlook.com>
2023-08-04 21:00:45 -0300

    Testing

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class HomePage extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
     return Scaffold(
       backgroundColor: Colors.green,
       body: Column(
