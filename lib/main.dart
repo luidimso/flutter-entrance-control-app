88d92742f0009a1906a88bc74ea8b82fdb0e53c8
luidimso <luidi_mso@outlook.com>
2023-08-07 21:56:09 -0300

    Adding an wallpaper

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,8 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const HomePage(),
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -27,46 +28,36 @@ class HomePage extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            "Can go in!",
-            style: TextStyle(
-                fontSize: 30, color: Colors.white, fontWeight: FontWeight.w700),
-          ),
-          const Padding(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/wallpaper.webp"),
+              fit: BoxFit.cover
+          )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Can go in!",
+              style: TextStyle(
+                  fontSize: 30, color: Colors.white, fontWeight: FontWeight.w700),
+            ),
+            const Padding(
               padding: EdgeInsets.all(32),
               child: Text(
                 "0",
                 style: TextStyle(fontSize: 100, color: Colors.white),
               ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
                   onPressed: decrement,
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    fixedSize: const Size(100, 100),
-                    foregroundColor: Colors.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24)
-                    )
-                  ),
-                  child: const Text(
-                    "Got out",
-                    style: TextStyle(color: Colors.black, fontSize: 16),
-                  ),),
-              const SizedBox(
-                width: 32,
-              ),
-              TextButton(
-                  onPressed: increment,
                   style: TextButton.styleFrom(
                       backgroundColor: Colors.white,
                       fixedSize: const Size(100, 100),
@@ -76,12 +67,30 @@ class HomePage extends StatelessWidget {
                       )
                   ),
                   child: const Text(
-                    "Got in",
+                    "Got out",
                     style: TextStyle(color: Colors.black, fontSize: 16),
-                  ))
-            ],
-          )
-        ],
+                  ),),
+                const SizedBox(
+                  width: 32,
+                ),
+                TextButton(
+                    onPressed: increment,
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        fixedSize: const Size(100, 100),
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24)
+                        )
+                    ),
+                    child: const Text(
+                      "Got in",
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ))
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
